05a3e067c9adfecdb40679b543291d9fbc86aefa
5003385 <91500260+5003385@users.noreply.github.com>
2021-11-30 12:15:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
 int main(){
+   int numero;
+
+    int cont=0;
+    cin >> numero;
+
+    funzione(numero, cont);
+    if (cont==0) {
+        cout << "numero primo" << endl;
+    } else {
+        cout << "numero non primo" << endl;
+    }
   return 0;
 }
