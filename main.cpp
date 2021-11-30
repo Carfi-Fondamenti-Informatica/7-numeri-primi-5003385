366387c7036115dc0b4d33288b3eb35b379f1d80
5003385 <91500260+5003385@users.noreply.github.com>
2021-11-30 12:37:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-int main(){
-   int numero;
+int main() {
+    int numero;
 
     int cont=0;
     cin >> numero;
 
-    lib(numero, cont);
+    lib(numero, 1, cont);
     if (cont==0) {
         cout << "numero primo" << endl;
     } else {
         cout << "numero non primo" << endl;
     }
-  return 0;
+    return 0;
 }
