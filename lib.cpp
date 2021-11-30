75b0f71b523d991591a613cc455f2faefd80ee78
5003385 <91500260+5003385@users.noreply.github.com>
2021-11-30 12:20:37 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
 
+#include "lib.h"
+
+void lib (int a, int &cont){
+    int i=0;
+    int r=0;
+    i++;
+    if (a>1 & a>i){
+        r=a%i;
+        if(r==0){
+            cont++;
+        } else {
+            lib(a, cont);
+        }
+    }
+
+}
